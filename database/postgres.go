11aa5e14dd3c5b8af7bf5bdd2ea7de838b19bf57
nilsmuralles <murallesnils11@gmail.com>
2025-03-30 19:41:07 -0600

    feat: Dockerization for single command start

--- database/postgres.go
+++ database/postgres.go
@@ -10,7 +10,7 @@ import (
 var db *gorm.DB
 
 func ConnectToDataBase() *gorm.DB  {
-  dsn := "host=localhost user=usuario password=secret dbname=series_tracker port=5434 sslmode=disable TimeZone=UTC"
+  dsn := "host=db user=usuario password=secret dbname=series_tracker port=5432 sslmode=disable TimeZone=UTC"
   var err error
 
   db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
